b55de36c3b4b3b1536b3352a072ed7615f05885c
Ndzi Nfor <ndzinfor@gmail.com>
2023-09-28 14:03:12 +0100

    Add css to game and link with js functionality

--- index.js
+++ index.js
@@ -17,30 +17,30 @@ function playRound(computerSelection, playerSelection){
     console.log(`the computer's choice is ${computerSelection}`);
     console.log(`your choice is ${playerSelection}`);
     if (playerSelection == computerSelection){
-        console.log(`You both selected ${playerSelection}, hence, a tie`)
+        return (`You both selected ${playerSelection}, hence, a tie`)
     } 
     else if (playerSelection == "rock"){
         if(computerSelection == "scissors"){
-            console.log(`You win`)
+            return (`You win! Rock covers scissors`)
         }
         else{
-            console.log("Oops! Paper covers Rock, so you loose")
+            return ("Oops! Paper covers Rock, so you loose")
         }
     }
     else if (playerSelection == "paper"){
         if(computerSelection == "rock"){
-            console.log("Great! Paper covers Rock, so you win. ")
+            return ("Great! Paper covers Rock, so you win. ")
         }
         else{
-            console.log(`Oops! Scissors cuts paper, so you lose`)
+            return (`Oops! Scissors cuts paper, so you lose`)
         }
     }
     else if (playerSelection == "scissors"){
         if(computerSelection == "paper"){
-            console.log("Great! Scissors cuts Paper, so you win")
+            return ("Great! Scissors cuts Paper, so you win")
         }
         else{
-            console.log("Oops! Rock smashes Scissors so you lose");
+            return ("Oops! Rock smashes Scissors so you lose");
         }
     }
 
@@ -50,10 +50,34 @@ function game(n){
     
     for(let i = 0; i<n; i++){
         let computerSelection = getComputerChoice(1,3);
-        let playerSelection = prompt("Please enter your choice from either Rock, Scissors or Paper")
+        // let playerSelection = prompt("Please enter your choice from either Rock, Scissors or Paper")
+        let playerSelection = playerChoice()
         playerSelection = playerSelection.toLowerCase();
         playRound(playerSelection, computerSelection)
     }
 }
 
-game(5);
+
+
+let btns = document.querySelectorAll('button');
+
+for (i of btns) {
+        i.addEventListener('click', function() {
+            if (this.id === "btns"){
+                let computerSelection = getComputerChoice(1,3);
+                let playerSelection = "scissors";
+                document.getElementById('result').innerText = playRound(computerSelection, playerSelection);
+            } else if (this === "btnp"){
+                let computerSelection = getComputerChoice(1,3);
+                let playerSelection = "paper";
+                document.getElementById('result').innerText = playRound(computerSelection, playerSelection);
+            } else{
+                let computerSelection = getComputerChoice(1,3);
+                let playerSelection = "rock";
+                document.getElementById('result').innerText = playRound(computerSelection, playerSelection);
+            }
+        //     document.getElementById('result').innerText = this.id
+        // // playerChoice(choice)
+            
+        });
+}
